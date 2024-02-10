cffb1c82fee9a400c0d750fb39d038bfaff34407
Utkarsh-1541027 <121924420+Utkarsh-1541027@users.noreply.github.com>
2024-02-10 15:07:43 +0530

    Update About.js

--- Urban-Yogi-main/src/pages/About/About.js
+++ Urban-Yogi-main/src/pages/About/About.js
@@ -28,17 +28,7 @@ export default function About() {
                     We have trained the model in python because of tensorflowJS we can leverage the support of browser so I converted 
                     the keras/tensorflow model to tensorflowJS.
                 </p>
-                {/* <div className="developer-info">
-                    <h4>About Developer</h4>
-                    <p className="about-content">I am Harsh, I am Full Stack Developer, AI Enthusiastic, Content Creator, Tutor,
-                        I love to work with technology and love to share on my youtube channel, 
-                        I hope this project will help you. 
-                    </p>
-                    <h4>Contact</h4>
-                    <a href="https://www.instagram.com/codedharsh75/"><p className="about-content">Instagram</p></a>
-                    <a href="https://www.youtube.com/channel/UCiD7kslR7lKSaPGSQ-heOWg"><p  className="about-content">Youtube</p></a>
-                    <a href="https://github.com/harshbhatt7585"><p  className="about-content">GitHub</p></a>
-                </div> */}
+                
             </div>
         </div>
     )
